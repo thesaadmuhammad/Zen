54f441d9cd9c6c0c460234cd05371bfb20e18d3f
anuradha008 <anuradha08vidhale@gmail.com>
2024-04-07 00:30:42 +0530

    src update

--- Zen/App.js
+++ Zen/App.js
@@ -1,30 +1,30 @@
-import 'react-native-gesture-handler'
-import { StyleSheet, Text, View } from 'react-native'
-import React from 'react'
+import 'react-native-gesture-handler';
+import {StyleSheet, Text, View} from 'react-native';
+import React from 'react';
 
-import { NavigationContainer ,useNavigation } from '@react-navigation/native';
-import { createDrawerNavigator  } from '@react-navigation/drawer';
-import { createNativeStackNavigator} from "@react-navigation/native-stack"
-import HomeScreen from './screen/HomeScreen';
-import AnxityLevelScreen from './screen/AnxityLevelScreen';
+import {NavigationContainer, useNavigation} from '@react-navigation/native';
+import {createDrawerNavigator} from '@react-navigation/drawer';
+import {createNativeStackNavigator} from '@react-navigation/native-stack';
+import HomeScreen from './src/screen/HomeScreen';
+import AnxityLevelScreen from './src/screen/AnxityLevelScreen';
 
-//Screens: 
-
-
+//Screens:
 
 const App = () => {
   const Stack = createNativeStackNavigator();
- 
+
   return (
     <NavigationContainer>
-       <Stack.Navigator initialRouteName='HomeScreen' screenOptions={{headerShown:false}}>
-        <Stack.Screen name='HomeScreen' component={HomeScreen} />
-        <Stack.Screen name='AnxityLevelScreen' component={AnxityLevelScreen} />
-       </Stack.Navigator> 
+      <Stack.Navigator
+        initialRouteName="HomeScreen"
+        screenOptions={{headerShown: false}}>
+        <Stack.Screen name="HomeScreen" component={HomeScreen} />
+        <Stack.Screen name="AnxityLevelScreen" component={AnxityLevelScreen} />
+      </Stack.Navigator>
     </NavigationContainer>
-  )
-}
+  );
+};
 
-export default App
+export default App;
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({});
---
+++ Zen/src/screen/HomeScreen.js
@@ -0,0 +1,127 @@
+import {StyleSheet, Text, View, TouchableOpacity, Image} from 'react-native';
+import React from 'react';
+
+import Icon from 'react-native-vector-icons/MaterialIcons';
+import MaterialCommunityIcons from 'react-native-vector-icons/MaterialCommunityIcons';
+import EntypoIcons from 'react-native-vector-icons/Entypo';
+import Header from '../../components/Header';
+const HomeScreen = ({navigation}) => {
+  return (
+    <View style={styles.container}>
+      {/* <Header
+        title='Home'
+        navigation={navigation}/> */}
+      <View style={styles.Header}>
+        <View style={styles.HeaderText}>
+          <Text style={styles.selectText}>Home</Text>
+          <Text style={styles.HeadText}>Sakha</Text>
+        </View>
+        <TouchableOpacity onPress={() => navigation.goBack()}>
+          <Icon name="timelapse" size={40} color="black" />
+        </TouchableOpacity>
+      </View>
+      <View style={styles.body}>
+        <Image
+          style={styles.homeImage}
+          source={require('../images/homeScreen.jpg')}
+        />
+        <Text style={styles.bodyText}>{'How You Feeling Today ?'}</Text>
+        <View style={styles.line}></View>
+        <View style={{marginVertical: '10%'}}>
+          <TouchableOpacity
+            style={styles.tabs}
+            onPress={() => navigation.navigate('AnxityLevelScreen')}>
+            <MaterialCommunityIcons
+              name="emoticon-sad-outline"
+              size={29}
+              color="black"
+            />
+            <Text style={styles.tabText}>{'Anxious'}</Text>
+          </TouchableOpacity>
+          <TouchableOpacity
+            style={styles.tabs}
+            onPress={() => navigation.navigate('AnxityLevelScreen')}>
+            <EntypoIcons name="emoji-sad" size={25} color="black" />
+            <Text style={styles.tabText}>{'Stressed'}</Text>
+          </TouchableOpacity>
+        </View>
+      </View>
+    </View>
+  );
+};
+
+export default HomeScreen;
+
+const styles = StyleSheet.create({
+  container: {
+    height: '100%',
+    backgroundColor: '#F5F5DC',
+    flex: 1,
+  },
+  Header: {
+    height: '10%',
+    flexDirection: 'row',
+    padding: 18,
+    justifyContent: 'space-between',
+  },
+  HeaderText: {
+    flexDirection: 'row',
+  },
+  selectText: {
+    fontSize: 25,
+    color: 'black',
+    fontWeight: 'bold',
+    marginHorizontal: 10,
+    marginTop: 3,
+    paddingBottom: 2,
+    borderBottomWidth: 3,
+    borderColor: '#CBA46F',
+  },
+  HeadText: {
+    color: 'black',
+    marginHorizontal: 10,
+    marginTop: 9,
+    fontSize: 20,
+    fontWeight: '500',
+  },
+  body: {
+    height: '100%',
+    marginLeft: 25,
+    marginTop: 25,
+    // backgroundColor:"red"
+  },
+  homeImage: {
+    width: 331,
+    height: 221,
+    borderRadius: 110,
+    marginTop: 25,
+  },
+  bodyText: {
+    marginTop: '10%',
+    fontSize: 18,
+    color: 'black',
+  },
+  line: {
+    height: 1,
+    backgroundColor: 'black',
+    width: 250,
+    marginTop: 15,
+  },
+  tabs: {
+    flexDirection: 'row',
+    width: '85%',
+    height: 50,
+    borderRadius: 16,
+    paddingHorizontal: 20,
+    // justifyContent: "space-between",
+    alignItems: 'center',
+    marginVertical: 10,
+    backgroundColor: '#FFFFFF',
+  },
+  tabText: {
+    fontWeight: 'bold',
+    fontSize: 18,
+    marginLeft: 10,
+    color: 'black',
+  },
+});
