98891e781196273dfd7d417bb5bf38e4763a385a
anuradha008 <anuradha08vidhale@gmail.com>
2024-04-06 21:17:03 +0530

    Navigation and HomeScreen

--- Zen/App.js
+++ Zen/App.js
@@ -1,11 +1,27 @@
+import 'react-native-gesture-handler'
 import { StyleSheet, Text, View } from 'react-native'
 import React from 'react'
 
+import { NavigationContainer ,useNavigation } from '@react-navigation/native';
+import { createDrawerNavigator  } from '@react-navigation/drawer';
+import { createNativeStackNavigator} from "@react-navigation/native-stack"
+import HomeScreen from './screen/HomeScreen';
+import AnxityLevelScreen from './screen/AnxityLevelScreen';
+
+//Screens: 
+
+
+
 const App = () => {
+  const Stack = createNativeStackNavigator();
+ 
   return (
-    <View>
-      <Text>Hello EveryOne </Text>
-    </View>
+    <NavigationContainer>
+       <Stack.Navigator initialRouteName='HomeScreen' screenOptions={{headerShown:false}}>
+        <Stack.Screen name='HomeScreen' component={HomeScreen} />
+        <Stack.Screen name='AnxityLevelScreen' component={AnxityLevelScreen} />
+       </Stack.Navigator> 
+    </NavigationContainer>
   )
 }
 
---
+++ Zen/screen/AnxityLevelScreen.js
@@ -0,0 +1,14 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+
+const AnxityLevelScreen = () => {
+  return (
+    <View>
+      <Text>AnxityLevelScreen</Text>
+    </View>
+  )
+}
+
+export default AnxityLevelScreen
+
+const styles = StyleSheet.create({})
