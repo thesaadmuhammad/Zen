b27ad2d597a43ca10fcab01699022bbe8252e999
Darshan Rahate <heyycreazy@gmail.com>
2024-04-07 08:49:25 +0530

    SakhaScreen

--- Zen/src/screens/SakhaScreen.js
+++ Zen/src/screens/SakhaScreen.js
@@ -1,12 +1,104 @@
-import React from 'react';
-import {Text, View, StyleSheet, TouchableOpacity} from 'react-native';
-import Icon from 'react-native-vector-icons/MaterialIcons';
+import {
+  Text,
+  View,
+  StyleSheet,
+  ScrollView,
+  Pressable,
+  TextInput,
+} from 'react-native';
 import Header from '../../src/components/Header.js';
+import React, {useState} from 'react';
+import {SafeAreaProvider} from 'react-native-safe-area-context';
+import {GoogleGenerativeAI} from '@google/generative-ai';
+import Icon from 'react-native-vector-icons/FontAwesome';
+
+const genAI = new GoogleGenerativeAI('AIzaSyCXH0pDnwUA_7MS2134pPqDtojnJMDYfLA');
 
 const SakhaScreen = () => {
+  const [text, setText] = useState('');
+
+  const [messages, setMessages] = useState([]);
+
+  const Card = ({message}) => {
+    return (
+      <View style={styles.card}>
+        <Text>{message}</Text>
+      </View>
+    );
+  };
+
+  async function handleSendPress(message) {
+    console.log(message);
+    setMessages(prevMessages => [...prevMessages, message]);
+    const chatbotResponse = await chatbotInteraction(message);
+    console.log(chatbotResponse.text());
+    setMessages(prevMessages => [...prevMessages, chatbotResponse.text()]);
+  }
+
+  async function chatbotInteraction(userInput) {
+    let counter = 5;
+    try {
+      const model = genAI.getGenerativeModel({model: 'gemini-pro'});
+      const chat = model.startChat({
+        history: [
+          {
+            role: 'user',
+            parts: [
+              {
+                text: 'Hello, You are my Stress, anxiety and depression therapist.',
+              },
+            ],
+          },
+          {
+            role: 'model',
+            parts: [{text: 'Great to meet you. How are you feeling today?'}],
+          },
+        ],
+        generationConfig: {
+          maxOutputTokens: 100,
+        },
+      });
+      const result = await chat.sendMessage(userInput);
+      let response = await result.response;
+      if (!response.text() && counter > 0) {
+        response = chatbotInteraction(userInput);
+        counter--;
+      }
+
+      return response || 'Okay I am listening you tell me more about it..';
+    } catch (error) {
+      return error;
+    }
+  }
+
   return (
     <View style={styles.container}>
-      <Header title="" navigation={'navigation'} />
+      <SafeAreaProvider>
+        <Header title="सखा" navigation={'navigation'} />
+
+        <ScrollView style={styles.scrollView}>
+          {messages.map((message, index) => (
+            <Card key={index} message={message} />
+          ))}
+        </ScrollView>
+        <View style={styles.row}>
+          <TextInput
+            style={{height: 40, paddingTop: 10}}
+            placeholder="Type your message here.."
+            onChangeText={newText => setText(newText)}
+            value={text}
+          />
+
+          <Pressable
+            style={styles.send}
+            onPress={() => {
+              handleSendPress(text);
+              setText('');
+            }}>
+            <Icon name="send" size={20} color="#000" />
+          </Pressable>
+        </View>
+      </SafeAreaProvider>
     </View>
   );
 };
@@ -17,101 +109,31 @@ const styles = StyleSheet.create({
     backgroundColor: '#F5F5DC',
     flex: 1,
   },
-  Header: {
-    height: '12%',
-    flexDirection: 'row',
-    padding: 18,
-    justifyContent: 'space-between',
-  },
-  HeaderText: {
-    flexDirection: 'row',
-  },
-  selectText: {
-    fontSize: 25,
-    color: 'black',
-    fontWeight: 'bold',
-    marginHorizontal: 10,
-    marginTop: 3,
-    paddingBottom: 2,
-    borderBottomWidth: 3,
-    borderColor: '#CBA46F',
-  },
-  HeadText: {
-    color: 'black',
-    marginHorizontal: 10,
-    marginTop: 9,
-    fontSize: 20,
-    fontWeight: '500',
-  },
-  Card: {
-    alignSelf: 'center',
-    height: '35%',
-    width: '85%',
+  card: {
+    alignSelf: 'flex-end',
+    width: '50%',
     backgroundColor: '#CBA46F',
     borderRadius: 10,
     marginHorizontal: 10,
     marginVertical: 25,
     padding: 10,
   },
-  text1: {
-    fontSize: 20,
-    fontWeight: 'bold',
-    color: 'black',
-    marginVertical: 10,
-  },
-  text2: {
-    fontSize: 25,
-    fontWeight: 'bold',
-    color: 'black',
-    marginVertical: 10,
-  },
-  text3: {
-    fontSize: 20,
-    color: 'black',
-    marginVertical: 10,
-    paddingTop: 4,
-  },
   row: {
+    height: 60,
+    borderTopWidth: 1,
+    position: 'absolute',
+    bottom: 0,
     flexDirection: 'row',
-    justifyContent: 'center',
+    justifyContent: 'space-between',
+    paddingHorizontal: 20,
+    width: '100%',
   },
-  text4: {
-    fontSize: 15,
-    color: 'black',
-    marginVertical: 10,
-    alignSelf: 'center',
+
+  scrollView: {
+    height: '100%',
   },
-  whiteButton: {
-    width: '40%',
-    height: '20%',
-    backgroundColor: '#F5F5DC',
-    borderRadius: 10,
-    alignSelf: 'center',
-    justifyContent: 'center',
-    marginVertical: '10%',
-  },
-  beigeButton: {
-    width: '40%',
-    height: '7%',
-    backgroundColor: '#D2B48C',
-    borderRadius: 10,
-    alignSelf: 'center',
-    justifyContent: 'center',
-    marginVertical: '10%',
-  },
-  text5: {
-    fontSize: 25,
-    color: 'black',
-    marginTop: 50,
-    fontWeight: '500',
-    alignSelf: 'center',
-  },
-  text6: {
-    fontSize: 20,
-    color: 'black',
-    marginVertical: 10,
-    alignSelf: 'center',
-    fontWeight: '500',
+  send: {
+    paddingTop: 10,
   },
 });
 
